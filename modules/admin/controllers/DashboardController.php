39f8282c1c42f2c1000e9010a71dd19fdf655f23
unknown <jis.nadim@gmail.com>
2023-10-14 19:00:01 +0530

    changes

--- modules/admin/controllers/DashboardController.php
+++ modules/admin/controllers/DashboardController.php
@@ -28,21 +28,6 @@ class DashboardController extends Controller
 	public function actionIndex()
 	{
 
-	$total_users = User::find()->where(['status' => User::STATUS_ACTIVE])->andWhere(['user_role' => User::ROLE_USER])->count();
-	$total_skippers = User::find()->where(['status' => User::STATUS_ACTIVE])->andWhere(['user_role' => User::ROLE_SKIPPER])->count();
-	$total_ride_request = RideRequest::find()->count();
-	$new_ride_request = RideRequest::find()->where(['status'=>RideRequest::STATUS_NEW_REQUEST])->count();
-	$total_completed_rides = RideRequest::find()->where(['status'=>RideRequest::STATUS_RIDE_COMPLETED_PAID])->count();
-	$total_earnings = RideEarnings::find()->sum('admin_earning');
-		return $this->render('index',['total_users' => $total_users,
-		'total_users' => $total_users,
-		'total_skippers' => $total_skippers,
-		'total_ride_request' => $total_ride_request,
-		'new_ride_request' => $new_ride_request,
-		'total_completed_rides' => $total_completed_rides,
-		'total_earnings' => $total_earnings,
-
-
-		]);
+		return $this->render('index');
 	}
 }
